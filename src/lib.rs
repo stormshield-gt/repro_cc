ac70da159c0e039f9c5b3ae02e491c3411786199
stormshield-gt <143998166+stormshield-gt@users.noreply.github.com.>
2024-09-23 10:50:50 +0200

    Latest cc, aws-lc-rs 1.9, macos, cache

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,9 @@ pub extern "C" fn repro() {
     let rng = ring::rand::SystemRandom::new();
     agreement::EphemeralPrivateKey::generate(&agreement::X25519, &rng).unwrap();
 }
+
+pub extern "C" fn repro2() {
+    use aws_lc_rs::agreement;
+    let rng = aws_lc_rs::rand::SystemRandom::new();
+    agreement::EphemeralPrivateKey::generate(&agreement::X25519, &rng).unwrap();
+}
