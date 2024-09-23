a8a0fba246fe6fca3c3c0f1f9ccb598293438b2f
stormshield-gt <143998166+stormshield-gt@users.noreply.github.com.>
2024-09-23 14:45:50 +0200

    Latest cc, uniffi, ios
    Latest cc, uniffi

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
-pub extern "C" fn repro() {
+#[uniffi::export]
+fn repro() {
     use ring::agreement;
     let rng = ring::rand::SystemRandom::new();
     agreement::EphemeralPrivateKey::generate(&agreement::X25519, &rng).unwrap();
 }
+
+#[uniffi::export]
+fn repro2() {
+    use aws_lc_rs::agreement;
+    let rng = aws_lc_rs::rand::SystemRandom::new();
+    agreement::EphemeralPrivateKey::generate(&agreement::X25519, &rng).unwrap();
+}
+
+uniffi::setup_scaffolding!();
