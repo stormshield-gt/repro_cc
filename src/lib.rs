dccdf815b7fe74cbb55be488c434da0002924525
stormshield-gt <143998166+stormshield-gt@users.noreply.github.com.>
2024-09-23 17:23:46 +0200

    Disable ring

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,11 @@ fn repro() {
     agreement::EphemeralPrivateKey::generate(&agreement::X25519, &rng).unwrap();
 }
 
-#[uniffi::export]
-fn repro2() {
-    use aws_lc_rs::agreement;
-    let rng = aws_lc_rs::rand::SystemRandom::new();
-    agreement::EphemeralPrivateKey::generate(&agreement::X25519, &rng).unwrap();
-}
+// #[uniffi::export]
+// fn repro2() {
+//     use aws_lc_rs::agreement;
+//     let rng = aws_lc_rs::rand::SystemRandom::new();
+//     agreement::EphemeralPrivateKey::generate(&agreement::X25519, &rng).unwrap();
+// }
 
 uniffi::setup_scaffolding!();
