67924cbdf1b6fc494aa5fd508783d8c61f7061b8
stormshield-gt <143998166+stormshield-gt@users.noreply.github.com.>
2024-09-25 10:51:04 +0200

    Add tracing os log

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
+use tracing_oslog::OsLogger;
+use tracing_subscriber::layer::SubscriberExt;
+
 #[uniffi::export]
 fn repro() {
     use ring::agreement;
+    let collector =
+        tracing_subscriber::registry().with(OsLogger::new("moe.absolucy.test", "default"));
+    tracing::subscriber::set_global_default(collector).expect("failed to set global subscriber");
+    tracing::info!("foo");
     let rng = ring::rand::SystemRandom::new();
     agreement::EphemeralPrivateKey::generate(&agreement::X25519, &rng).unwrap();
 }
